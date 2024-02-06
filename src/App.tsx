85f01cd3bbd044061f5bd3954da3574657fa4944
tsteelematc <tsteele@madisoncollege.edu>
2024-02-06 13:34:09 -0600

    Added rating control...
    Saw gap-1, mt-5, -red-400... Classes, classes, classes
    
    : - )

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App p-3">
       <button className="btn btn-primary">Hello daisyUI</button>
-      <div className="card bg-base-100 shadow-xl">
+      <div className="card bg-base-100 shadow-xl mt-5">
         <div className="card-body">
           <h2 className="card-title">Card title!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -14,6 +14,13 @@ function App() {
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
+      </div>
+      <div className="rating gap-1 mt-5">
+        <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
       </div>
     </div>
   );
