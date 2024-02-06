b3338b79320a5a680b075005e16d3e29105ddfb6
tsteelematc <tsteele@madisoncollege.edu>
2024-02-06 13:28:24 -0600

    A card component...
    Possibly a ui-hammer...
    
    p-3 for app padding
    
    no w-96, to keep the width down on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-3">
       <button className="btn btn-primary">Hello daisyUI</button>
+      <div className="card bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
