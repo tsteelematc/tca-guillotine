e58407e9c65ce3833c74391baa318b5e596eae03
tsteelematc <tsteele@madisoncollege.edu>
2024-02-08 12:57:00 -0600

    Initial navigation working...
    Next let's do something other than our `<h3>`'s

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <h3>Home</h3>
+    },
+    {
+      path: "/setup",
+      element: <h3>Setup</h3>
+    },
+    {
+      path: "/play",
+      element: <h3>Play</h3>
+    },
+  ]);  
+
+
   return (
-    <div className="App p-3">
-      <button className="btn btn-primary">Hello daisyUI</button>
-      <div className="card bg-base-100 shadow-xl mt-5">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="rating gap-1 mt-5">
-        <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
-      </div>
+    <div 
+      className="App p-3"
+    >
+      <RouterProvider 
+        router={router} 
+      />
     </div>
   );
 }
