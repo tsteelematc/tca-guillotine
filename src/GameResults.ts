81fc9b4e71d901890a35db2696e9d35c6397c0f9
tsteelematc <tsteele@madisoncollege.edu>
2024-02-15 13:02:03 -0600

    Finished harvesting for today...
    Had to change TS options in order to spread a set into an array literal...

--- src/GameResults.ts
+++ src/GameResults.ts
@@ -7,7 +7,67 @@ export type GameResult = {
     players: string[];
 };
 
+export type LeaderboardEntry = {
+    wins: number;
+    losses: number;
+    avg: number;
+    name: string;
+};
 
 //
-// Functions...
+// Exported functions...
 //
+
+export const getPreviousPlayers = (results: GameResult[]) => {
+
+    const previousPlayers = results.flatMap(
+        x => x.players
+    );
+
+    return [
+        ...new Set(previousPlayers)
+    ].sort(
+        (a, b) => a.localeCompare(b)
+    );
+
+};
+
+export const getLeaderboard = (results: GameResult[]): LeaderboardEntry[] => {
+    
+    const players = getPreviousPlayers(results);
+    
+    return players.map(
+        x => getLeaderboardEntryForPlayer(results, x)
+    ).sort(
+        // (a, b) => b.avg - a.avg
+
+        // i-o-g
+        (a, b) => (b.avg * 1000 + b.wins + b.losses) - (a.avg * 1000 + a.wins + a.losses)
+    );
+};
+
+
+//
+// Internal functions...
+//
+
+const getLeaderboardEntryForPlayer = (results: GameResult[], player: string): LeaderboardEntry => {
+
+    const playerWins = results.filter(x => x.winner === player).length;
+    const playerGames = results.filter(
+        x => x.players.some(
+            y => y === player
+        )
+    ).length;
+
+    return {
+        wins: playerWins
+        , losses: playerGames - playerWins
+
+        , avg: playerGames > 0
+            ? playerWins / playerGames
+            : 0
+            
+        , name: player
+    };
+};
