489c259874e3fca3032e22454aaaa0621459fc5f
tsteelematc <tsteele@madisoncollege.edu>
2024-02-20 13:30:35 -0600

    Squashed commit of the following:
    commit 50a1dac38e765454417455c95202f58a490fd23c
    Author: tsteelematc <tsteele@madisoncollege.edu>
    Date:   Tue Feb 20 13:23:25 2024 -0600
    
        Rounding is easy ! ! !
    
    commit 8d4493665f64058ce108e7dacf04bf9228505f23
    Author: tsteelematc <tsteele@madisoncollege.edu>
    Date:   Tue Feb 20 13:19:27 2024 -0600
    
        Thing showing up...
    
        Let's fix the rounding next ! ! !
    
    commit e5b928b7376631c69790a74d043de78493758d79
    Author: tsteelematc <tsteele@madisoncollege.edu>
    Date:   Tue Feb 20 13:05:20 2024 -0600
    
        Got ternary working to tell user to play a game
    
    commit 9b86388bfc3550fffdfd7dd2e14226df5a037a71
    Author: tsteelematc <tsteele@madisoncollege.edu>
    Date:   Tue Feb 20 12:59:01 2024 -0600
    
        Remembered to use Array.map to stamp out JSX ! ! !
    
    commit acc63db151c95f07d48446f32bec14b07f99292f
    Author: tsteelematc <tsteele@madisoncollege.edu>
    Date:   Tue Feb 20 12:52:49 2024 -0600
    
        Starting find/remember some ui-hammers...
    
        Cards FTW ! ! !

--- src/Home.tsx
+++ src/Home.tsx
@@ -23,6 +23,53 @@ export const Home: FC<HomeProps> = ({ leaderboardData }) => {
             >
                 Play
             </button>
+            <div
+                className='card mt-3 bg-base-100 shadow-xl'
+            >
+                <div
+                    className='card-body'
+                >
+                    <h2
+                        className='card-title'
+                    >
+                        Leaderboard
+                    </h2>
+                    {
+                        leaderboardData.length > 0
+                        ? (
+                            <table
+                                className='table'
+                            >
+                                <thead>
+                                    <tr>
+                                        <th>W</th>
+                                        <th>L</th>
+                                        <th>AVG</th>
+                                        <th>PLAYER</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        leaderboardData.map(lbe => (
+                                            <tr>
+                                                <td>{ lbe.wins }</td>
+                                                <td>{ lbe.losses }</td>
+                                                <td>{ lbe.avg.toFixed(3) }</td>
+                                                <td>{ lbe.name }</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        )
+                        : (
+                            <p>
+                                Play a game to see the leaderboard!
+                            </p>
+                        )
+                    }
+                </div>
+            </div>
         </>
     );
 };
