5fddbe9024df983aacd0160e3c27505529d1b76f
tsteelematc <tsteele@madisoncollege.edu>
2024-02-21 15:03:13 -0600

    Kind of fun : - )

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import { Home } from './Home';
+import { Home, AppTitle } from './Home';
 import { Setup } from './Setup';
 import { Play } from './Play';
 import { GameResult, getLeaderboard } from './GameResults';
@@ -58,6 +58,8 @@ const App = () => {
   // const [gameResults, setGameResults] = useState<GameResult[]>([]);
   
   const [gameResults, setGameResults] = useState<GameResult[]>(dummyGameResults);
+  
+  const [title, setTitle] = useState(AppTitle)
 
   const addNewGameResult = (result: GameResult) => setGameResults(
     [
@@ -71,28 +73,51 @@ const App = () => {
       path: "/",
       element: <Home
         leaderboardData={getLeaderboard(gameResults)}
+        setTitle={setTitle}
       />
     },
     {
       path: "/setup",
-      element: <Setup />
+      element: <Setup 
+        setTitle={setTitle}
+      />
     },
     {
       path: "/play",
       element: <Play 
           addNewGameResult={addNewGameResult}
-        />
+          setTitle={setTitle}
+          />
     },
   ]);  
 
 
   return (
     <div 
-      className="App p-3"
+      className="App"
     >
-      <RouterProvider 
-        router={router} 
-      />
+      <nav
+        className='navbar bg-base-300'
+      >
+        {
+          title === AppTitle &&
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+          </svg>
+        }
+        <span
+          className='text-lg font-bold ml-3'
+        >
+          { title }
+        </span>
+      </nav>
+      <div
+        className='p-3'
+      >
+        <RouterProvider 
+          router={router} 
+        />
+      </div>
     </div>
   );
 }
--- src/Play.tsx
+++ src/Play.tsx
@@ -1,25 +1,30 @@
 import { useNavigate } from 'react-router-dom';
 import { GameResult } from './GameResults';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 interface PlayProps {
   addNewGameResult: (result: GameResult) => void;
+  setTitle: (t: string) => void;
 }
 
-export const Play: FC<PlayProps> = ({ addNewGameResult }) => {
+export const Play: FC<PlayProps> = ({ addNewGameResult, setTitle }) => {
 
+    useEffect(
+      () => setTitle("Play Guillotine")
+      , []
+    );
+    
     const nav = useNavigate();
 
     return (
-      <>
-        <h3>
-          Play
-        </h3>
+      <div
+        className='flex flex-col gap-3'
+      >
         <p>
           Play the game and tap the app ! ! !
         </p>
         <button
-            className="btn btn-outline btn-primary"
+            className="btn btn-lg btn-primary"
             onClick={() => {
               addNewGameResult({
                 winner: "Tom"
@@ -33,6 +38,6 @@ export const Play: FC<PlayProps> = ({ addNewGameResult }) => {
         >
             Done
         </button>
-      </>
+      </div>
     );
   };
