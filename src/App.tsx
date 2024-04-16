d8d77880d50eb319f6e21edbd4499956a919b766
tsteelematc <tsteele@madisoncollege.edu>
2024-04-16 16:04:05 -0500

    Blah, lots of code, review before teaching...
    : - )

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import {
   , getAverageGameDurationsByPlayerCount
 } from './GameResults';
 import { loadGamesFromCloud, saveGameToCloud } from './tca-cloud-api';
+import localforage from 'localforage';
 
 const App = () => {
 
@@ -32,6 +33,10 @@ const App = () => {
 
   const [darkMode, setDarkMode] = useState(false);
 
+  const [dialogEmail, setDialogEmail] = useState("");
+  
+  const [cloudApiEmail, setCloudApiEmail] = useState("");
+
   const dialog = useRef<HTMLDialogElement | null>(null);
 
   useEffect(
@@ -39,12 +44,25 @@ const App = () => {
       const init = async () => {
 
         if (!ignore) {
-          const cloudGameResults = await loadGamesFromCloud(
-            "tsteele@madisoncollege.edu"
-            , "tca-guillotine-24s"
-          );
 
-          setGameResults(cloudGameResults);
+          const darkMode = await localforage.getItem<boolean>("darkMode");
+          setDarkMode(darkMode ?? false);
+          
+          const savedEmail = await localforage.getItem<string>("email") ?? "";
+
+          if (savedEmail.length > 0) {
+            setCloudApiEmail(savedEmail);
+            setDialogEmail(savedEmail);
+
+            const cloudGameResults = await loadGamesFromCloud(
+              savedEmail
+              , "tca-guillotine-24s"
+            );
+            setGameResults(cloudGameResults);  
+          }
+          else {
+            dialog?.current?.showModal();
+          }
         }
       };
 
@@ -55,7 +73,7 @@ const App = () => {
         ignore = true;
       };
     }
-    , []
+    , [cloudApiEmail]
   );
 
   const addNewGameResult = async (result: GameResult) => {
@@ -131,16 +149,19 @@ const App = () => {
         <div
           className='ml-auto mr-3'
         >
-          <button 
-            className="mr-6"
-            onClick={() => dialog?.current?.showModal()}
-          >
-            {/* https://heroicons.com/ search gear, copy JSX */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-          </button>
+          {
+            title === AppTitle &&          
+            <button
+              className="mr-6 mb-1"
+              onClick={() => dialog?.current?.showModal()}
+            >
+              {/* https://heroicons.com/ search gear, copy JSX */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            </button>
+          }
           <div
             className=''
           >
@@ -150,7 +171,10 @@ const App = () => {
               <input
                 type="checkbox"
                 checked={darkMode}
-                onChange={() => setDarkMode(!darkMode)}
+                onChange={async () => {
+                  await localforage.setItem<boolean>("darkMode", !darkMode);
+                  setDarkMode(!darkMode);
+                }}
               />
 
               {/* sun icon */}
@@ -172,19 +196,49 @@ const App = () => {
         />
       </div>
 
-      <dialog ref={dialog} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click the button below to close</p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+      <dialog
+        ref={dialog}
+        id="email_dialog"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div
+          className="modal-box"
+        >
+          <h3
+            className="font-bold text-lg"
+          >
+            Enter email for saving game results...
+          </h3>
+          <div
+            className='mt-3'
+          >
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="input input-bordered w-full"
+              value={dialogEmail}
+              onChange={(e) => setDialogEmail(e.target.value)}
+          />
+          </div>
+          <div
+            className="modal-action"
+          >
+            <form
+              method="dialog"
+            >
+              <button
+                className="btn btn-primary ml-3"
+                onClick={async () => {
+                  await localforage.setItem<string>("email", dialogEmail);
+                  setCloudApiEmail(dialogEmail);
+                }}
+              >
+                Done
+              </button>
             </form>
           </div>
         </div>
       </dialog>
-
     </div>
   );
 }
